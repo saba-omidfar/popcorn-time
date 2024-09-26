7ad78ff879cc530ecfebc3ae1bf65812c0aef775
saba omidfar <saba.omidfar68@gmail.com>
2024-09-26 08:23:07 +0330

    change vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/popcorn-time/", // Adjust this to match your GitHub Pages subpath
   build: {
     outDir: "dist",
     rollupOptions: {
       input: {
         main: "src/main.jsx",
+        vendor: "src/vendor/index.js",
       },
     },
   },
